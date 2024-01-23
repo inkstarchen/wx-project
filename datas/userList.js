50311b04a429c868c664933d7bd9b6025c4301af
chenguanglun <3190100029@zju.edu.cn>
2024-01-23 18:32:14 +0800

    更新课程页面

---
+++ datas/userList.js
@@ -0,0 +1,35 @@
+var json = [
+  {
+      "title": "播放列表"
+  },
+  {
+      "title": "已下载"
+  },
+  {
+      "title": "已收藏歌曲"
+  }
+]
+
+var broad = [
+  {
+      name: "资料",
+      thumb: "https://d.musicapp.migu.cn/data/oss/service66/00/2b/b4/0n"
+  },
+  {
+      name: "历年卷",
+      thumb: "https://d.musicapp.migu.cn/data/oss/service66/00/2b/2z/te"
+  },
+  {
+      name: "笔记",
+      thumb: "https://d.musicapp.migu.cn/data/oss/service66/00/2b/b3/zb"
+  },
+  {
+      name: "读物推荐",
+      thumb: "https://d.musicapp.migu.cn/data/oss/service66/00/2b/b4/0t_m.webp"
+  }
+]
+
+module.exports = {
+  users: json,
+  latest: broad
+}
--- pages/course/course.js
+++ pages/course/course.js
@@ -1,66 +1,47 @@
-// pages/course/course.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+import userList from "../../datas/userList.js";
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+Page ({
   data: {
+    users: [],
+    broads: [],
+
+    userInfo: {
+      avatarUrl: defaultAvatarUrl,
+      nickName: '',
+    },
+    hasUserInfo: false,
+    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+onLoad: function(options) {
+    this.setData({
+      users: userList.users,
+      broads: userList.latest
+    });
+    console.log(userList);
+    console.log(userList.latest);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  userInfo: {
+    avatarUrl: defaultAvatarUrl,
+    nickName: '',
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  hasUserInfo: false,
+  canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+  canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  onChooseAvatar(e) {
+    const { avatarUrl } = e.detail
+    const { nickName } = this.data.userInfo
+    this.setData({
+      "userInfo.avatarUrl": avatarUrl,
+      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
