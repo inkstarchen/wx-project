c8748103d89c31fc28531aa181ba0ebed17ddb6d
44331155 <155537142+44331155@users.noreply.github.com>
2024-01-24 16:30:58 +0800

    新增了search和tests页面以及相关的data、component文件夹

---
+++ components/courseList/courseList.js
@@ -0,0 +1,29 @@
+// components/courseList/courseList.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    item:{
+      type:Object,
+      value:{}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    tapCourse:function(e){
+      console.log("tap course!")
+    }
+  }
+})
