3853ad7bf757bbc6df8ef32ecbf6b76ca77397f4
Inkstar <147968499+inkstarchen@users.noreply.github.com>
2024-01-24 22:30:49 +0800

    基本页面整合版本更新

--- miniprogram-1/components/latestItem/latestItem.js
+++ miniprogram-1/components/latestItem/latestItem.js
@@ -37,7 +37,33 @@ Component({
         wx.navigateTo({
           url: '/pages/resource/resource',
         })
-      }
+      }var json = [
+
+]
+
+var broad = [
+  {
+      name: "资料",
+      thumb: "https://d.musicapp.migu.cn/data/oss/service66/00/2b/b4/0n"
+  },
+  {
+      name: "历年卷",
+      thumb: "https://d.musicapp.migu.cn/data/oss/service66/00/2b/2z/te"
+  },
+  {
+      name: "笔记",
+      thumb: "https://d.musicapp.migu.cn/data/oss/service66/00/2b/b3/zb"
+  },
+  {
+      name: "读物推荐",
+      thumb: "https://d.musicapp.migu.cn/data/oss/service66/00/2b/b4/0t_m.webp"
+  }
+]
+
+module.exports = {
+  users: json,
+  latest: broad
+}
       
   }
   }
